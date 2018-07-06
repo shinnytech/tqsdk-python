2ebda9caaa1020e03ff1e40ea394343ba1f43a7f
chengzhi <chengzhi@shinnytech.com>
2018-07-06 09:35:06 +0800

    fix setup script

--- setup.py
+++ setup.py
@@ -1,21 +1,27 @@
 # -*- coding: utf-8 -*-
 __author__ = 'yangyang'
 
-from setuptools import setup, find_packages
-# from tqsdk import __version__
+import setuptools
 
+with open("README.rst", "r") as fh:
+    long_description = fh.read()
 
-setup(name='tqsdk',
-      version="0.8.0",
-      description='TianQin ',
-      author='tq18.cn',
-      author_email='tianqincn@gmail.com',
-      url='http://www.tq18.cn/',
-      packages=find_packages(exclude=["tqsdk.test"]),
-      zip_safe=False,
-      include_package_data=True,
-      package_data={
-      },
-      python_requires='>=3.6',
-      install_requires=["websockets>=5.0.1"],
-      )
+setuptools.setup(
+    name='tqsdk',
+    version="0.8.0",
+    description='TianQin SDK',
+    author='TianQin',
+    author_email='tianqincn@gmail.com',
+    long_description=long_description,
+    long_description_content_type="text/x-rst",
+    url='http://www.shinnytech.com/tianqin',
+    packages=setuptools.find_packages(exclude=["tqsdk.test"]),
+    zip_safe=False,
+    python_requires='>=3.6',
+    install_requires=["websockets>=5.0.1"],
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ),
+)
