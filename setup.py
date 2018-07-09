a86985ecf992d5de344df8b52d2e9f080efa8c4c
chengzhi <chengzhi@shinnytech.com>
2018-07-09 14:43:51 +0800

    bump version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.rst", "r") as fh:
 
 setuptools.setup(
     name='tqsdk',
-    version="0.8.0",
+    version="0.8.1",
     description='TianQin SDK',
     author='TianQin',
     author_email='tianqincn@gmail.com',
