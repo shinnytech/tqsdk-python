fc9da225556fb5dda4b68d57896e353e7527f4f2
snake3342 <snake3342@qq.com>
2018-08-08 18:23:53 +0800

    "增加上期所优先平今or平昨的选项"

--- tqsdk/lib.py
+++ tqsdk/lib.py
@@ -6,23 +6,20 @@ from tqsdk.api import TqChan
 
 class TargetPosTask:
     """目标持仓 task, 该 task 可以将指定合约调整到目标头寸"""
-    def __init__(self, api, symbol, price="ACTIVE", init_pos=0, init_pos_today=0, trade_chan=None):
-        """
+	def __init__(self, api, symbol, price="ACTIVE",init_pos=0, trade_chan=None, settle_yesterday_first=True):
+		"""
         创建目标持仓task实例
 
         Args:
             api (TqApi): TqApi实例，该task依托于指定api下单/撤单
-
             symbol (str): 负责调整的合约代码
-
             price (str): [可选]下单方式, ACTIVE=对价下单, PASSIVE=挂价下单
-
             init_pos (int): [可选]初始持仓，默认为0 (全部持仓)
-            
             init_pos_today (int): [可选]初始持仓，默认为0 (此字段只有上期所合约有效: 持今仓)
-            
             trade_chan (TqChan): [可选]成交通知channel, 当有成交发生时会将成交手数(多头为正数，空头为负数)发到该channel上
+			settle_yesterday_first(bool):是否优先平昨
         """
+		self.settle_yes_first=settle_yesterday_first
         self.api = api
         self.symbol = symbol
         self.price = price
@@ -31,6 +28,59 @@ class TargetPosTask:
         self.pos_chan = TqChan(last_only=True)
         self.trade_chan = trade_chan if trade_chan is not None else TqChan()
         self.task = self.api.create_task(self._target_pos_task())
+		
+		
+	def shfe_settle(self, delta_volume):
+		'''平上海仓'''
+		
+		target_direction_type="SELL" if delta_volume>0 else "BUY"
+		operations_to_go=[]
+		position_status=self.api.get_position(self.symbol)
+		
+		#昨今多空可用仓
+		yes_long_available=position_status['volume_long_his']-position_status['volume_long_frozen']
+		yes_short_available=position_status['volume_short_his']-position_status['volume_short_frozen']
+		today_long_available=position_status['volume_long_today']-position_status['volume_long_frozen_today']
+		today_short_available=position_status['volume_short_today']-position_status['volume_short_frozen_today']
+		
+		#读取优先仓
+		if self.settle_yes_first:  #本品种优先平昨
+			prior_available_position= yes_short_available if delta_volume>0 else yes_long_available
+			prior_offset="CLOSE"
+			secondary_offset="CLOSETODAY"
+		else:  #本品种优先平今
+			prior_available_position=today_short_available if delta_volume>0 else today_long_available
+			prior_offset="CLOSETODAY"
+			secondary_offset="CLOSE"
+		
+		#次要仓
+		secondary_position=abs(delta_volume)-prior_available_position
+		if secondary_position>0:  #优先仓不够平
+			secondary_operation=InsertOrderUntilAllTradedTask(self.api, self.symbol,target_direction_type,secondary_offset,secondary_position, price=self.price,trade_chan=self.trade_chan)
+			prior_operation=InsertOrderUntilAllTradedTask(self.api, self.symbol,target_direction_type,prior_offset,prior_available_position, price=self.price,trade_chan=self.trade_chan)
+			if prior_available_position>0:  #解决优先仓没有持仓，只平次要仓的情况
+				operations_to_go.append(prior_operation)
+			operations_to_go.append(secondary_operation)
+		else:  #优先仓够平
+			operations_to_go.append(InsertOrderUntilAllTradedTask(self.api, self.symbol,target_direction_type,prior_offset,abs(delta_volume), price=self.price,trade_chan=self.trade_chan))
+		
+		return operations_to_go
+		
+	def open_position(self, delta_volume):
+		'''开仓'''
+		long_short=(delta_volume>0)  #开多还是开空
+		insert_order_until_all_traded_task=InsertOrderUntilAllTradedTask(self.api, self.symbol,"BUY" if long_short else "SELL", "OPEN",abs(delta_volume), price=self.price,trade_chan=self.trade_chan)
+		return [insert_order_until_all_traded_task]
+		
+	def settle_position(self, delta_volume):
+		'''平仓'''
+		target_direction_type="SELL" if delta_volume>0 else "BUY"  #大于0平多，小于0平空
+		
+		if not self.symbol.startswith("SHFE"):#非上期所
+			return [InsertOrderUntilAllTradedTask(self.api, self.symbol, target_direction_type, "CLOSE", abs(delta_volume),
+										  price=self.price, trade_chan=self.trade_chan)]
+		else:
+			return self.shfe_settle(delta_volume)
 
     def set_target_volume(self, volume):
         """
@@ -52,8 +102,30 @@ class TargetPosTask:
                 target_pos.set_target_volume(5)
         """
         self.pos_chan.put_nowait(volume)
-
-    async def _target_pos_task(self):
+			
+	async def _target_pos_task(self):
+		"""负责调整目标持仓的task,根据构造函数的选项决定上期所先平昨还是先平今，默认平昨"""
+		current_pos=self.init_pos
+		async for target_pos in self.pos_chan:
+		
+			delta_volume=target_pos-current_pos
+			
+			operation_list_to_go=[]
+			
+			if (current_pos>=0 and delta_volume>0) or (current_pos<=0 and delta_volume<0):  #增多仓或者增空仓
+				operation_list_to_go.extend(self.open_position(delta_volume))
+				pass
+			
+			if (0<=target_pos<current_pos) or (0>=target_pos>current_pos):  #减多仓或者减空仓
+				operation_list_to_go.extend(self.settle_position(-delta_volume,current_pos))
+				pass
+			if target_pos>0>current_pos or target_pos<0<current_pos:  #空换多或者多换空
+				operation_list_to_go.extend(self.settle_position(current_pos))
+				operation_list_to_go.extend(self.open_position(target_pos))
+				pass
+			
+			await gather(*[each.task for each in operation_list_to_go])
+			current_pos+=delta_volume
         """
         负责调整目标持仓的task
         上期所区分平今平昨，先平今仓
