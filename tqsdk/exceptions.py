d7363e825d47dcadc39fb7f86c62d0b2fbae1e6e
chengzhi <chengzhi@shinnytech.com>
2019-01-22 09:23:04 +0800

    backtest: catch uncaught BacktestFinished exception and exit program

--- tqsdk/exceptions.py
+++ tqsdk/exceptions.py
@@ -2,8 +2,23 @@
 #  -*- coding: utf-8 -*-
 __author__ = 'chengzhi'
 
+import sys
+
+
 class BacktestFinished(Exception):
     """回测结束会抛出此例外"""
+
+    _orig_excepthook = None
+
     def __init__(self):
         message = "回测结束"
         super().__init__(message)
+        if BacktestFinished._orig_excepthook is None:
+            BacktestFinished._orig_excepthook = sys.excepthook
+
+            def _except_catcher(type, value, traceback):
+                if type is BacktestFinished:
+                    sys.exit()
+                BacktestFinished._orig_excepthook(type, value, traceback)
+
+            sys.excepthook = _except_catcher
