4fb75e0f73d9318af69256040228db2a4be27864
tianqin <tianqin@shinnytech.com>
2019-11-18 10:33:31 +0800

    修正文档语言

--- doc/conf.py
+++ doc/conf.py
@@ -57,7 +57,7 @@ release = u'1.2.0'
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = "zh_CN"
+language = "zh"
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
