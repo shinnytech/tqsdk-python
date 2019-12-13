4424ba267e759496655982929fc2ffe56f149958
limin <weeeest@qq.com>
2019-12-13 10:30:25 +0800

    测试用例：移除 unittest.TestCase 子测试类以外的测试函数
    当测试文件中含有继承自 unittest.TestCase 的测试类，在测试类以外不应存在其他测试函数。若存在，在 Pycharm 中运行此测试文件时无法运行到这个测试函数（在 pytest 中能测试到），当遇到错误则无法对其进行debug。
    
    close #215

---
+++ tqsdk/test/api/test_md_backtest.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+#  -*- coding: utf-8 -*-
+__author__ = 'limin'
+
+import json
+from datetime import datetime
+from tqsdk import TqApi, TqBacktest
+
+
+def test_get_quote_normal_backtest():
+    """
+    回测获取行情报价
+
+    """
+    # 获取行情
+    api = TqApi(backtest=TqBacktest(datetime(2019, 10, 15), datetime(2019, 10, 16)),
+                _ins_url="https://openmd.shinnytech.com/t/md/symbols/2019-07-03.json")
+    quote = api.get_quote("SHFE.cu2001")
+    quote_data = {k: v for k, v in quote.items()}
+    quote_data["trading_time"] = {k: v for k, v in quote_data["trading_time"].items()}
+
+    assert json.dumps(quote_data, sort_keys=True) == \
+           '{"amount": NaN, "ask_price1": 47070.0, "ask_volume1": 1, "average": NaN, "bid_price1": 47050.0, "bid_volume1": 1, "close": NaN, "commission": 11.594999999999999, "datetime": "2019-10-14 23:59:59.999999", "delivery_month": 1, "delivery_year": 2020, "expire_datetime": 1579071600.0, "expired": false, "highest": NaN, "ins_class": "FUTURE", "last_price": 47060.0, "lower_limit": NaN, "lowest": NaN, "margin": 16233.000000000002, "max_limit_order_volume": 500, "max_market_order_volume": 0, "min_limit_order_volume": 0, "min_market_order_volume": 0, "open": NaN, "open_interest": 90714, "pre_close": NaN, "pre_open_interest": 0, "pre_settlement": NaN, "price_decs": 0, "price_tick": 10, "settlement": NaN, "strike_price": NaN, "trading_time": {"day": [["09:00:00", "10:15:00"], ["10:30:00", "11:30:00"], ["13:30:00", "15:00:00"]], "night": [["21:00:00", "25:00:00"]]}, "underlying_symbol": "", "upper_limit": NaN, "volume": 0, "volume_multiple": 5}'
+
+    # 其他取值方式
+    assert quote["pre_close"] != quote.pre_close
+    assert quote.get("pre_settlement") != quote.pre_settlement
+    assert quote.get("highest") != quote.highest
+    assert quote.get("lowest") != quote.lowest
+    assert quote["open"] != quote.open
+    assert quote["close"] != quote.close
+    api.close()
