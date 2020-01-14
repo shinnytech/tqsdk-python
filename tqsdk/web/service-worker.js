1d8cebbd0af49bc6f3cb4b9c6f6cf423ef7d5a6b
Elizabeth <ma_yanqiong@163.com>
2020-01-14 18:30:48 +0800

    fix: #270

--- tqsdk/web/service-worker.js
+++ tqsdk/web/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.94bb041166cc6db9355cdf7adf6478eb.js"
+  "/precache-manifest.30c552be0f625f705a12c10135b71e1e.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "shinny-futures-web"});
