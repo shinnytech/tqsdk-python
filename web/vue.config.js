96292c002dd9006ef63f3a0cf69d5c35caf8dc26
Elizabeth <ma_yanqiong@163.com>
2020-01-21 17:53:04 +0800

    fix: 更新 web 打包脚本，输出脚本目录为 tqsdk/web

--- web/vue.config.js
+++ web/vue.config.js
@@ -1,35 +1,14 @@
 module.exports = {
-  pages: {
-    index: {
-      entry: 'src/main.js',
-      template: 'public/index.html',
-      filename: 'index.html',
-      // chunks to include on this page, by default includes extracted common chunks and vendor chunks.
-      chunks: ['chunk-vendors', 'chunk-common', 'index']
-    }
-  },
+  outputDir: '../tqsdk/web',
   lintOnSave: false,
   productionSourceMap: false,
   chainWebpack: config => {
-    if(process.env.NODE_ENV ===  'production' && config.plugins.has('extract-css')) {
-      const extractCSSPlugin = config.plugin('extract-css')
-      extractCSSPlugin && extractCSSPlugin.tap(() => [{
-        filename: '[name].[contenthash:8].css',
-        chunkFilename: '[name].[contenthash:8].css'
-      }])
+    if (process.env.NODE_ENV ===  'production') {
+      config.optimization.minimizer('terser').tap((args) => {
+        args[0].terserOptions.compress.drop_console = true
+        return args
+      })
     }
   },
-  configureWebpack: {
-    // output: {
-    //   filename: '[name].[chunkhash:8].js',
-    //   chunkFilename: '[name].[chunkhash:8].js'
-    // },
-    optimization: {
-      splitChunks: {
-        chunks: 'all'
-        // minSize: 10000,
-        // maxSize: 250000,
-      }
-    }
-  }
+  configureWebpack: {}
 }
