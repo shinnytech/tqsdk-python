d2ba0d275097478b97de9c01332544751dc1e315
shinny-mayanqiong <mayanqiong@shinnytech.com>
2020-03-16 23:12:07 +0800

    ta: 利用隐含波动率计算GREEKS;BS_PRICE,GREEKS,VALUES,IMP_VOLATILITY增加quote参数;增加HIS_VOLATILITY

--- tqsdk/demo/ta_option.py
+++ tqsdk/demo/ta_option.py
@@ -5,20 +5,25 @@ from tqsdk import TqApi
 from tqsdk.ta import *
 
 api = TqApi()
-option = api.get_quote("CFFEX.IO2012-C-4000")
-r = 0.025  # 无风险利率
-klines = api.get_kline_serial(["CFFEX.IO2012-C-4000", "CSI.000300"], 24 * 60 * 60, 30)
 
-values = VALUES(klines)
-print("==== 内在价值 时间价值 ====")
-print(values.iloc[-1:])
-bs_serise = BS_PRICE(klines, option.expire_datetime, r)
-print("==== 理论价格 ====")
-print(bs_serise.iloc[-1:])
-impv = IMPV(klines, option.expire_datetime, r)
-print("==== 隐含波动率 ====")
-print(impv.iloc[-1:])
-greeks = GREEKS(klines, option.expire_datetime, r)
-print("==== 希腊指标 ====")
-print(greeks.iloc[-1:])
+underlying_quote = api.get_quote("SHFE.cu2006")
+klines = api.get_kline_serial('SHFE.cu2006', 24 * 60 * 60, 20)
+v = HIS_VOLATILITY(klines, underlying_quote)
+print("历史波动率:", v)
+
+quote = api.get_quote("SHFE.cu2009C44000")
+klines2 = api.get_kline_serial(["SHFE.cu2009C44000", "SHFE.cu2006"], 24 * 60 * 60, 20)
+
+values = VALUES(klines2, quote)
+print(values)
+
+bs_serise = BS_PRICE(klines2, quote, 0.025)
+print("理论价:", list(round(bs_serise['bs_price'], 2)))
+
+impv = IMP_VOLATILITY(klines2, quote, 0.025)
+print("隐含波动率:", list(round(impv['impv'] * 100, 2)))
+
+greeks = GREEKS(klines2, quote, 0.025, impv['impv'])
+print(greeks)
+
 api.close()
