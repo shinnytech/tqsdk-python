a5776293faf3b3f82a59be9bf178741a352db1d4
shinny-limin <limin@shinnytech.com>
2020-04-09 18:19:29 +0800

    api,objs: 去掉order中的冻结权利金和冻结保证金
    ctp返回的数据中没有这两个字段

--- tqsdk/api.py
+++ tqsdk/api.py
@@ -655,8 +655,6 @@ class TqApi(object):
             "price_type": "ANY" if limit_price is None else "LIMIT",
             "volume_condition": "ANY",
             "time_condition": "IOC" if limit_price is None else "GFD",
-            "frozen_premium": 0.0,
-            "frozen_margin": 0.0,
         })
         return order
 
