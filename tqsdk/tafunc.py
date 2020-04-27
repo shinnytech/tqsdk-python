47db04c22a6741ba96529759523a029763d1b847
shinny-mayanqiong <mayanqiong@shinnytech.com>
2020-04-27 11:04:46 +0800

    tafunc: 增加计算 ticks 开平方向函数

--- tqsdk/tafunc.py
+++ tqsdk/tafunc.py
@@ -1316,3 +1316,66 @@ def get_impv(series, series_option, k, r, init_v, t, option_class):
         vega = get_vega(series, k, r, x, t)
         diff_x = pd.Series(np.where(np.isnan(vega) | (vega < 1e-8), np.nan, (series_option - y) / vega))
     return x
+
+
+def get_ticks_info(df):
+    """
+    计算 ticks 开平方向
+
+    Args:
+        df (pandas.DataFrame): Dataframe 格式的 ticks 序列
+
+    Returns:
+        pandas.Series: 返回序列的开平方向序列
+
+    Example::
+
+        from tqsdk import TqApi, tafunc
+
+        api = TqApi()
+        ticks = api.get_tick_serial('SHFE.cu2006', 100)
+        ticksinfo = tafunc.get_ticks_info(ticks)
+        for i, v in ticksinfo.items():
+            if v:
+                print(f"{tafunc.time_to_str(ticks['datetime'][i])[5:21]}   {ticks['last_price'][i]}   {v}")
+        api.close()
+
+        # 预计的输出是这样的:
+        04-27 10:54:11.5   42640.0   多换
+        04-27 10:54:12.0   42640.0   多换
+        04-27 10:54:16.5   42640.0   多换
+        ......
+        04-27 10:55:10.0   42660.0   双平
+        04-27 10:55:10.5   42660.0   双平
+        04-27 10:55:14.0   42670.0   双平
+    """
+    if "open_interest" not in df.keys():  # df 不是 ticks 是 klines
+        raise Exception(f"get_ticks_info 参数必须是 ticks，由 api.get_tick_serial 返回的对象。")
+
+    df_pre = df.copy().shift(1)[1:]
+    df_pre["price_diff"] = df["last_price"][1:] - df_pre["last_price"]
+    df_pre["oi_diff"] = df["open_interest"][1:] - df_pre["open_interest"]
+    df_pre["vol_diff"] = df["volume"][1:] - df_pre["volume"]
+    df_pre["pc"] = np.array(np.where(df["last_price"][1:] >= df_pre["ask_price1"], 1,
+                                     np.where(df["last_price"][1:] <= df_pre["bid_price1"], -1,
+                                              np.sign(df_pre["price_diff"]))))
+
+    def get_from_tick(t):
+        if not t["vol_diff"]:
+            return ""
+        elif t["oi_diff"] > 0 and t["oi_diff"] == t["vol_diff"]:
+            return "双开"
+        elif t["oi_diff"] < 0 and t["oi_diff"] + t["vol_diff"] == 0:
+            return "双平"
+        elif t["pc"] == 0:
+            return "换手"
+        elif t["oi_diff"] > 0:
+            return ("多" if t["pc"] > 0 else "空") + "开"
+        elif t["oi_diff"] < 0:
+            return ("多" if t["pc"] < 0 else "空") + "平"
+        else:
+            return ("多" if t["pc"] > 0 else "空") + "换"
+
+    ticksinfo = df_pre.apply(get_from_tick, axis=1)
+    ticksinfo.at[0] = ""
+    return ticksinfo.sort_index()
