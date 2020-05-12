324212306875bdd716b067df536ebeec4aa7cfb3
shinny-mayanqiong <mayanqiong@shinnytech.com>
2020-05-12 16:31:11 +0800

    bump version 1.8.0beta

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ with open("README.md", mode="r", encoding='utf-8') as fh:
 
 setuptools.setup(
     name='tqsdk',
-    version="1.7.0",
+    version="1.8.0beta",
     description='TianQin SDK',
     author='TianQin',
     author_email='tianqincn@gmail.com',
