6edd5146f08831ae3786fc02c2e7ad2472aa6065
shinny-mayanqiong <mayanqiong@shinnytech.com>
2020-05-15 10:54:44 +0800

    doc: update copyright

--- doc/conf.py
+++ doc/conf.py
@@ -40,7 +40,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'TianQin Python SDK'
-copyright = u'2018-2019, TianQin'
+copyright = u'2018-2020, TianQin'
 author = u'TianQin'
 
 # The version info for the project you're documenting, acts as replacement for
