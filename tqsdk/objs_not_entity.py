4095751cc7273277e241f21ee2e7fe87f19d0a26
shinny-pack <shinny-pack@users.noreply.github.com>
2021-09-06 12:33:23 +0000

    Update Version 2.8.5

--- tqsdk/objs_not_entity.py
+++ tqsdk/objs_not_entity.py
@@ -3,13 +3,16 @@
 __author__ = 'mayanqiong'
 
 from collections import namedtuple
-from typing import Callable, Union, Tuple
+from datetime import datetime
+from typing import Callable, Tuple
 
-from pandas import DataFrame
+import aiohttp
+from pandas import DataFrame, Series
 
 from tqsdk.objs import Quote
 from tqsdk.diff import _get_obj
 from tqsdk.utils import _query_for_quote, query_all_fields, _generate_uuid
+from tqsdk.tafunc import _get_t_series, get_impv, _get_d1, get_delta, get_theta, get_gamma, get_vega, get_rho
 
 """
 这两个类只在 api 中用到，主要为了支持用户异步中 await 
@@ -269,6 +272,182 @@ class TqSymbolDataFrame(DataFrame):
         default_quote = Quote(None)
         for col in self.__dict__["_columns"]:
             self.loc[:, col] = [quotes[s].get(col, default_quote[col]) for s in self.__dict__["_symbol_list"]]
+
+    def __await__(self):
+        return self.__dict__["_task"].__await__()
+
+
+class TqSymbolRankingDataFrame(DataFrame):
+
+    def __init__(self, api, symbol, ranking_type, days, start_dt, broker):
+        self.__dict__["_api"] = api
+        params = {'symbol': symbol}
+        if days is not None:
+            params['days'] = days
+        if start_dt is not None:
+            params['start_date'] = start_dt.strftime("%Y%m%d")
+        if broker is not None:
+            params['broker'] = broker
+        self.__dict__["_params"] = params
+        self.__dict__["_symbol"] = symbol
+        self.__dict__["_ranking_type"] = f"{ranking_type.lower()}_ranking"
+        self.__dict__["_columns"] = [
+            "datetime",
+            "symbol",
+            "exchange_id",
+            "instrument_id",
+            "broker",
+            "volume",
+            "volume_change",
+            "volume_ranking",
+            "long_oi",
+            "long_change",
+            "long_ranking",
+            "short_oi",
+            "short_change",
+            "short_ranking"
+        ]
+        super(TqSymbolRankingDataFrame, self).__init__(data=[], columns=self.__dict__["_columns"])
+        self.__dict__["_task"] = api.create_task(self.async_update(), _caller_api=True)
+
+    async def _get_ranking_data(self, ranking_id):
+        # 下载持仓排名数据，并将数据发回到 api.recv_chan
+        async with aiohttp.ClientSession(headers=self.__dict__["_api"]._base_headers) as session:
+            url = "https://symbol-ranking-system-fc-api.shinnytech.com/srs"
+            async with session.get(url, params=self.__dict__["_params"]) as response:
+                response.raise_for_status()
+                content = await response.json()
+                await self.__dict__["_api"]._ws_md_recv_chan.send({
+                    "aid": "rtn_data",
+                    "data": [{
+                        "_symbol_rankings": {
+                            ranking_id: content
+                        }
+                    }]
+                })
+
+    async def async_update(self):
+        ranking_id = _generate_uuid("PYSDK_rank")
+        self.__dict__["_api"].create_task(self._get_ranking_data(ranking_id), _caller_api=True)  # 错误会抛给 api 处理
+        symbol_rankings = _get_obj(self.__dict__["_api"]._data, ["_symbol_rankings"])
+        async with self.__dict__["_api"].register_update_notify(symbol_rankings) as update_chan:
+            async for _ in update_chan:
+                content = symbol_rankings.get(ranking_id, None)
+                if content is None:
+                    continue
+                data = self._content_to_list(content)
+                for i, d in enumerate(data):
+                    self.loc[i] = d
+                self.dropna(subset=[self.__dict__["_ranking_type"]], inplace=True)
+                self.sort_values(by=['datetime', self.__dict__["_ranking_type"]], inplace=True, ignore_index=True)
+                # 读完数据，清空数据
+                await self.__dict__["_api"]._ws_md_recv_chan.send({
+                    "aid": "rtn_data",
+                    "data": [{
+                        "_symbol_rankings": {
+                            ranking_id: None
+                        }
+                    }]
+                })
+                return self
+
+    def _content_to_list(self, content):
+        data = {}
+        for dt in content.keys():
+            for symbol in content[dt].keys():
+                if content[dt][symbol] is None:
+                    continue
+                for data_type, rankings in content[dt][symbol].items():
+                    for broker, rank_item in rankings.items():
+                        item = data.setdefault((dt, symbol, broker), self._get_default_item(dt, symbol, broker))
+                        if data_type == 'volume_ranking':
+                            item['volume'] = rank_item['volume']
+                            item['volume_change'] = rank_item['varvolume']
+                            item['volume_ranking'] = rank_item['ranking']
+                        elif data_type == 'long_ranking':
+                            item['long_oi'] = rank_item['volume']
+                            item['long_change'] = rank_item['varvolume']
+                            item['long_ranking'] = rank_item['ranking']
+                        elif data_type == 'short_ranking':
+                            item['short_oi'] = rank_item['volume']
+                            item['short_change'] = rank_item['varvolume']
+                            item['short_ranking'] = rank_item['ranking']
+        return data.values()
+
+    def _get_default_item(self, dt, symbol, broker):
+        return {
+            "datetime": dt,
+            "symbol": symbol,
+            "exchange_id": symbol.split(".", maxsplit=1)[0],
+            "instrument_id": symbol.split(".", maxsplit=1)[1],
+            "broker": broker,
+            "volume": float('nan'),
+            "volume_change": float('nan'),
+            "volume_ranking": float('nan'),
+            "long_oi": float('nan'),
+            "long_change": float('nan'),
+            "long_ranking": float('nan'),
+            "short_oi": float('nan'),
+            "short_change": float('nan'),
+            "short_ranking": float('nan')
+        }
+
+    def __await__(self):
+        return self.__dict__["_task"].__await__()
+
+
+
+class TqOptionGreeksDataFrame(DataFrame):
+
+    def __init__(self, api, symbol_list, v_list, r):
+        self.__dict__["_api"] = api
+        self.__dict__["_symbol_list"] = symbol_list
+        self.__dict__["_v_list"] = v_list
+        self.__dict__["_r"] = r
+        self.__dict__["_columns"] = [
+            "instrument_id",
+            "instrument_name",
+            "option_class",
+            "expire_rest_days",
+            "expire_datetime",
+            "underlying_symbol",
+            "strike_price",
+            "delta",
+            "gamma",
+            "theta",
+            "vega",
+            "rho"
+        ]
+        super(TqOptionGreeksDataFrame, self).__init__(data=[], columns=self.__dict__["_columns"])
+        self.__dict__["_task"] = api.create_task(self.async_update(), _caller_api=True)
+
+    async def async_update(self):
+        symbol_list = self.__dict__["_symbol_list"]
+        quotes = await self.__dict__["_api"].get_quote_list(symbol_list)
+        if not all([q.ins_class.endswith("OPTION") for q in quotes]):
+            raise Exception("quote 参数列表中元素必须是期权类型")
+        for i, q in enumerate(quotes):
+            self.loc[i] = {k: q.get(k, float('nan')) for k in self.__dict__["_columns"]}
+        self._get_greeks(quotes)
+        return self
+
+    def _get_greeks(self, quotes):
+        series_close = Series(data=[q.last_price for q in quotes])  # 期权最新价
+        series_close1 = Series(data=[q.underlying_quote.last_price for q in quotes])  # 标的最新价
+        series_o = Series(data=[q.option_class for q in quotes])
+        series_datetime = Series(data=[datetime.strptime(q.datetime, "%Y-%m-%d %H:%M:%S.%f").timestamp() * 1000000000 for q in quotes])
+        series_expire_datetime = Series(data=[q.expire_datetime for q in quotes])
+        series_t = _get_t_series(series_datetime, 0, series_expire_datetime)  # 到期时间
+        if self.__dict__["_v_list"] is None:
+            series_v = get_impv(series_close1, series_close, self["strike_price"], self.__dict__["_r"], 0.3, series_t, series_o)
+        else:
+            series_v = Series(data=self.__dict__["_v_list"])
+        series_d1 = _get_d1(series_close1, self["strike_price"], self.__dict__["_r"], series_v, series_t)
+        self["delta"] = get_delta(series_close1, self["strike_price"], self.__dict__["_r"], series_v, series_t, series_o, series_d1)
+        self["theta"] = get_theta(series_close1, self["strike_price"], self.__dict__["_r"], series_v, series_t, series_o, series_d1)
+        self["gamma"] = get_gamma(series_close1, self["strike_price"], self.__dict__["_r"], series_v, series_t, series_d1)
+        self["vega"] = get_vega(series_close1, self["strike_price"], self.__dict__["_r"], series_v, series_t, series_d1)
+        self["rho"] = get_rho(series_close1, self["strike_price"], self.__dict__["_r"], series_v, series_t, series_o, series_d1)
 
     def __await__(self):
         return self.__dict__["_task"].__await__()
