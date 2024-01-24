873f19d2d1c29c8c8a5c9e8e09c3ae660a2619cd
shinny-pack <shinny-pack@users.noreply.github.com>
2024-01-24 07:20:32 +0000

    Update Version 3.5.1

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", mode="r", encoding='utf-8') as fh:
 
 setuptools.setup(
     name='tqsdk',
-    version="3.5.0",
+    version="3.5.1",
     description='TianQin SDK',
     author='TianQin',
     author_email='tianqincn@gmail.com',
